63664d9075feb1ee8a5ce5ad3ebff67aa9195c9c
uguribadov <uguribadov2012@gmail.com>
2026-02-13 21:19:37 +0400

    salaaam

---
+++ task/index.js
@@ -0,0 +1,13 @@
+console.log("Menim adım Uğur");
+
+console.warn("Menim soyadım Ibadov");
+
+console.error("Menim yaşım 14");
+
+// const
+// var
+// let
+
+// const user1 = "031"; 
+// let user2 = "031"; 
+// var user3 = "031"; 
