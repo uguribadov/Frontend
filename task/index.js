ad3006705d184cb784a70e803b396938d79f68da
uguribadov <uguribadov2012@gmail.com>
2026-02-13 22:02:57 +0400

    some creepy ahh changes

--- task/index.js
+++ task/index.js
@@ -1,8 +1,8 @@
-console.log("Menim adım Uğur");
+// console.log("Menim adım Uğur");
 
-console.warn("Menim soyadım Ibadov");
+// console.warn("Menim soyadım Ibadov");
 
-console.error("Menim yaşım 14");
+// console.error("Menim yaşım 14");
 
 // const
 // var
@@ -11,3 +11,13 @@ console.error("Menim yaşım 14");
 // const user1 = "031"; 
 // let user2 = "031"; 
 // var user3 = "031"; 
+/*
+const name = "Uğur";
+const surname = "Ibadov";
+const age = 14;
+console.log(name + " " + surname + " " + age);
+*/
+
+const bookPrice = 95;
+const bookAmount = 30;
+console.log(bookPrice * bookAmount);
